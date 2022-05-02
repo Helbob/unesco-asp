1b5b4d797ac97359ab4cda4af3789d0ed990ccc9
Helbob <millehelbo@outlook.dk>
2022-05-02 13:30:47 +0200

    titel

--- vorestema/page-baeredygtig-udvikling.php
+++ vorestema/page-baeredygtig-udvikling.php
@@ -19,7 +19,7 @@
    	  	<div class="row">
 	<main id="main" class="site-main">
 <!-- <nav id="filtrering"></nav> -->
-    <h1>Global Medborgerskab</h1>
+    <h1>Bæredygtig Udvikling</h1>
 <section class="retcontainer2">
 </section>
 </div>
--- vorestema/page-maal.php
+++ vorestema/page-maal.php
@@ -18,7 +18,7 @@
         <div id="content" class="container">
    	  	<div class="row">
 	<main id="main" class="site-main">
-    <h1>Global Medborgerskab</h1>
+    <h1>UNESCO Skolemål</h1>
 <section class="retcontainer4">
 </section>
 </div>
